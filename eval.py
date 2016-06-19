a7483667004e31a8ebd0056f8fa2da8069745a08
Pashutan Modaresi <p.modaresi@gmx.de>
2016-06-20 00:19:04 +0200

    adde a function to compute c@1 measure

---
+++ eval.py
@@ -0,0 +1,16 @@
+def c_at_one(answers):
+    '''Given an answers file, computes the c@1 measure
+
+    # Arguments
+        answers: file containing the probabilities for each problem
+
+    # Returns
+        c@1: the c@1 measure
+    '''
+    with open(answers) as f:
+        lines = f.readlines()
+    n = float(len(lines))
+    probs = [float(line.split()[1]) for line in lines]
+    n_c = sum(p < 0.5 for p in probs)
+    n_u = 0  # glad does not support this feature
+    return (1.0 / n) * (n_c + (n_u * n_c / n))
