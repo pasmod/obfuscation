072d085967182b7cc8c6619ade31c1203d1671cc
Pashutan Modaresi <p.modaresi@gmx.de>
2016-06-20 00:42:48 +0200

    removed the auc function

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import auc
+from sklearn import metrics
 
 
 def c_at_one(answers):
@@ -17,19 +17,3 @@ def c_at_one(answers):
     n_c = sum(p < 0.5 for p in probs)
     n_u = 0  # glad does not support this feature
     return (1.0 / n) * (n_c + (n_u * n_c / n))
-
-
-def auc(answers):
-    '''Returns the AUC based on the probabilities given in answers
-
-    # Arguments:
-        answers: file containing the probabilities for each problem
-
-    # Returns
-        auc: aread under the curve
-    '''
-    with open(answers) as f:
-        lines = f.readlines()
-    probs = [float(line.split()[1]) for line in lines]
-    y = [0] * len(probs)
-    return auc(probs, y)
