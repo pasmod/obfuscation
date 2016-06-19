f714a2fbedbc95c9fd085547efda72fa01fe9463
Pashutan Modaresi <p.modaresi@gmx.de>
2016-06-19 21:34:55 +0200

    added a function to write obfuscations as a single text file

--- reconstructor.py
+++ reconstructor.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def reconstruct(problem):
@@ -16,3 +17,27 @@ def reconstruct(problem):
         obfuscations = json.load(data_file)
     docs = [o['obfuscation'] for o in obfuscations]
     return "".join(docs)
+
+
+def write_obfuscation(problem):
+    """Combines the obfuscations in a problem to a single one and writes
+    the result to a text file
+
+    # Arguments
+        problem: problem containing the obfuscation.json file
+    """
+    obfuscation = reconstruct(problem)
+    f = open('obfuscation.txt', 'w')
+    f.write(obfuscation)
+    f.close()
+
+def write_obfuscations(path):
+    """For all problems in the path, write the obfuscation file to the folder
+
+    # Arguments
+        path: path containing the results of participants
+    """
+    for _, dirs, _ in os.walk(path):
+        for dir in dirs:
+            problam_path = os.path.join(path, dir)
+            write_obfuscation(problam_path)
