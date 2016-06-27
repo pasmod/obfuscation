f85ff424333af6cc0bada6943ff8970ecc5efcb8
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-06-27 11:45:05 +0200

    added the csv files of the manual evaluations

--- eval.py
+++ eval.py
@@ -112,3 +112,44 @@ def create_pairs(submission):
         the_file.write(pair[1])
         the_file.write('\n')
     the_file.close()
+
+
+def merge(file1, file2):
+    merged = []
+    with open(file1) as f1:
+         eval1= json.load(f1, object_pairs_hook=OrderedDict)
+    with open(file2) as f2:
+         eval2= json.load(f2, object_pairs_hook=OrderedDict)
+    for i in range(0, len(eval1)):
+        new_dic = {}
+        new_dic['obfuscation'] = eval1[i]['1_obfuscation']
+        new_dic['sensibleness'] = {'A': eval1[i]['2_sensibleness'], 'B': eval2[i]['2_sensibleness']}
+        new_dic['obfuscation-id'] = eval1[i]['3_obfuscation-id']
+        new_dic['original-end-charpos'] = eval1[i]['4_original-end-charpos']
+        new_dic['original-start-charpos'] = eval1[i]['5_original-start-charpos']
+        new_dic['problem'] = eval1[i]['problem']
+        new_dic['submission'] = eval1[i]['submission']
+        merged.append(new_dic)
+    filename = 'results/sensibleness_team_a.json'
+    json.dump(merged, open(filename, 'wb'), sort_keys=False, indent=4)
+
+
+def ia(file):
+    values = []
+    with open(file) as f:
+         evaluations= json.load(f, object_pairs_hook=OrderedDict)
+    for evaluation in evaluations:
+        a = evaluation['sensibleness']['A']
+        b = evaluation['sensibleness']['B']
+        values.append((a, b))
+    the_file = codecs.open("sensibleness_team_c.csv", "w", "utf-8")
+    for value in values:
+        the_file.write(value[0])
+        the_file.write(',')
+        the_file.write(value[1])
+        the_file.write('\n')
+    the_file.close()
+
+
+
+
